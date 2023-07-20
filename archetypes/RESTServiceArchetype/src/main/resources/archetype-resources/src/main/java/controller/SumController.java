45c28d960d8784769cfb573bbf0e429ca686c6f4
Lindbrum <ultrascati95@gmail.com>
2023-07-20 23:07:57 +0200

    Added custom API info bean

--- archetypes/RESTServiceArchetype/src/main/resources/archetype-resources/src/main/java/controller/SumController.java
+++ archetypes/RESTServiceArchetype/src/main/resources/archetype-resources/src/main/java/controller/SumController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import ${package}.model.Sum;
 import ${package}.service.SumService;
 import ${package}.service.SumServiceImpl;
@@ -24,6 +30,34 @@ public class SumController {
 		this.sumService = sumServiceImpl;
 	}
 
+	@Operation(summary = "Calculate the sum of two integers and returns a formatted message.")
+	@ApiResponses(value = { 
+		  @ApiResponse(
+				  responseCode = "200", 
+				  description = "The sum was calculated and the message template found.", 
+				  content = { 
+						  @Content(
+								  mediaType = "application/json", 
+								  schema = @Schema(implementation = Sum.class)
+								  ) 
+				  }),
+		  @ApiResponse(
+				  responseCode = "400",
+				  description = "Invalid addendum(s) provided", 
+				  content = {
+						  @Content(
+								  mediaType = "application/json"
+								  )
+				  }), 
+		  @ApiResponse(
+				  responseCode = "500",
+				  description = "Message template not found", 
+				  content = {
+						  @Content(
+								  mediaType = "application/json"
+								  )
+				  }) 
+	})
 	@GetMapping("/sum")
 	public Sum sum(@RequestParam(value = "arg1", defaultValue = "0") int arg1, 
 						@RequestParam(value = "arg2", defaultValue = "0") int arg2) {
