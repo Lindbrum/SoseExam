da432e89b28a895879a9b5380e8e7c9b3a2412ae
ddm18 <dario8090@gmail.com>
2023-07-22 03:18:45 +0200

    minor fix

--- flight-rest-service/src/main/java/it/univaq/dandd/controller/FlightController.java
+++ flight-rest-service/src/main/java/it/univaq/dandd/controller/FlightController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import jakarta.websocket.server.PathParam;
 import it.univaq.dandd.service.FlightRouteImpl;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightController {
 	
 	//Service (injected)
